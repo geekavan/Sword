7190c893a6d5a39a41a73a6756d3e7c2bb9cc432
Ivan <2937923559@qq.com>
2019-10-13 09:45:11 +0800

    面试题1515 一个数的二进制表示中1的个数

--- Sword.java
+++ Sword.java
@@ -1,4 +1,11 @@
 public class Sword{
+
+    /**
+     * 剑指Offer面试题16，求一个数的整数次方，不考虑大数问题
+     * @param base 底数
+     * @param exponent 指数
+     * @return 底数的指数次方
+     */
     static double power(double base, int exponent){
         if(base==0 && exponent==0)
             return 0;
@@ -12,5 +19,19 @@ public class Sword{
         }
         else
             return base*power(base, (exponent-1));
+    }
+
+    /**
+     * 剑指Offer面试题15，求一个数二进制表示中1的个数
+     * @param number 要求的二进制中1的个数的数字 
+     * @return  1的个数
+     */
+    static int numberOf1(int number){
+        int result = 0;
+        while(number!=0){
+            result++;
+            number = number&(number-1);
+        }
+        return result;
     }
 }
--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 class Test{
     public static void main(String[] args){
-        double s16  = Sword.power(5, -2);
-        System.out.println(s16);
+        // double s16  = Sword.power(5, -2);
+        // System.out.println(s16);
+
+        int s15  = Sword.numberOf1(127);
+        System.out.println(s15);
     }
 }
