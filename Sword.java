36724932acd2d1e43534e07348dafb8740655fea
Ivan <2937923559@qq.com>
2019-10-13 13:09:34 +0800

    面试题56

--- Sword.java
+++ Sword.java
@@ -34,4 +34,51 @@ public class Sword{
         }
         return result;
     }
+    
+    /**
+     * 找到一个数组中只出现一次的两个数字，其他数字都出现了两次
+     * @param arr 
+     * @param num1 用来存储两个数字之一
+     * @param num2 用来存储两个数字之一
+     */
+    static void findNumsApperOnce(int[] arr, int[] num1, int[] num2){
+        if(arr.length < 2)
+            return;
+        num1[0] = arr[0];
+        num2[0] = arr[1];
+        int temp = arr[0];
+        for(int i = 0;i<arr.length;i++){
+            temp = temp^arr[i];
+        }
+        int pos;
+        for(pos = 0;(temp&1)!=1;pos++)
+            temp = temp>>1;
+        for(int i=0;i<arr.length;i++){
+            if((arr[i]>>pos)==0)
+                num1[0]^=arr[i];
+            else
+                num2[0]^=arr[i];
+        }
+    }
+
+    /**
+     * 用查找一个数组中只出现过一次的数字，其他数字都出现了三次
+     * @param arr
+     * @return 返回该数字
+     */
+    static int findNumberApperingOnce(int[] arr){
+        int[] bit = new int[32];
+        for(int i=0;i<arr.length;i++){
+            int temp = 1;
+            for(int j=0;j<32;j++){
+                bit[j]+=(arr[i]&(temp<<j))>>>j;
+            }
+        }
+        int res = 0;
+        for(int i=0;i<32;i++){
+            if(bit[i]%3==1)
+                res+=(1<<i);
+        }
+        return res;
+    }
 }
--- Test.java
+++ Test.java
@@ -1,9 +1,25 @@
 class Test{
     public static void main(String[] args){
+        //面试题15
+        // int s15  = Sword.numberOf1(127);
+        // System.out.println(s15);
+
+        //面试题16
         // double s16  = Sword.power(5, -2);
         // System.out.println(s16);
 
-        int s15  = Sword.numberOf1(127);
-        System.out.println(s15);
+        /* 面试题56 题目一
+        int[] num1 = new int[1];
+        int[] num2 = new int[1];
+        int[] arr = {1,2,3,4,5,6,8,7,9,5,4,3,1,8,9,7};
+        Sword.findNumsApperOnce(arr, num1, num2);
+        System.out.println(num1[0]+"____"+num2[0]);
+        */
+
+        /* 面试题56 题目二
+        int[] arr = {1,1,1,2,2,2,3,3,3,6,6,6,9,9,9,8,8,8,7,7,7,4,2,2,2};
+        int num = Sword.findNumberApperingOnce(arr);
+        System.out.println(num);
+        */
     }
 }
