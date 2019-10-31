f75ab7ac585d1aad8f37d5720f772e91bcdf55bf
Ivan <2937923559@qq.com>
2019-10-31 20:40:42 +0800

    面试题21

--- Sword.java
+++ Sword.java
@@ -165,6 +165,68 @@ public class Sword{
         return array[rightIndex];
     }
 
+    
+    /**
+     * 剑指Offer面试题15，求一个数二进制表示中1的个数
+     * @param number 要求的二进制中1的个数的数字 
+     * @return  1的个数
+     */
+    public static int NumberOf1(int n) {
+        int numbers = 0;
+        while(n!=0){
+            numbers++;
+            n=n&(n-1);
+        }
+        return numbers;
+    }
+    
+    /**
+     * 面试题56_数组中数字出现的次数，一个整型数组里除两个数字之外，其他数字都出现了两次
+     * @param arr 
+     * @param num1 用来存储两个数字之一
+     * @param num2 用来存储两个数字之一
+     */
+    public static void findNumsApperOnce(int[] arr, int[] num1, int[] num2){
+        if(arr.length < 2)
+            return;
+            num1[0] = arr[0];
+        num2[0] = arr[1];
+        int temp = arr[0];
+        for(int i = 0;i<arr.length;i++){
+            temp = temp^arr[i];
+        }
+        int pos;
+        for(pos = 0;(temp&1)!=1;pos++)
+        temp = temp>>1;
+        for(int i=0;i<arr.length;i++){
+            if((arr[i]>>pos)==0)
+            num1[0]^=arr[i];
+            else
+            num2[0]^=arr[i];
+        }
+    }
+    
+    /**
+     * 面试题56_数组中数字出现的次数，题目二：用查找一个数组中只出现过一次的数字，其他数字都出现了三次
+     * @param arr
+     * @return 返回该数字
+     */
+    public static int findNumberApperingOnce(int[] arr){
+        int[] bit = new int[32];
+        for(int i=0;i<arr.length;i++){
+            int temp = 1;
+            for(int j=0;j<32;j++){
+                bit[j]+=(arr[i]&(temp<<j))>>>j;
+            }
+        }
+        int res = 0;
+        for(int i=0;i<32;i++){
+            if(bit[i]%3==1)
+            res+=(1<<i);
+        }
+        return res;
+    }
+
     /**
      * 剑指Offer面试题16，求一个数的整数次方，不考虑大数问题
      * @param base 底数
@@ -184,66 +246,5 @@ public class Sword{
         }
         else
             return base*power(base, (exponent-1));
-    }
-
-    /**
-     * 剑指Offer面试题15，求一个数二进制表示中1的个数
-     * @param number 要求的二进制中1的个数的数字 
-     * @return  1的个数
-     */
-    public static int numberOf1(int number){
-        int result = 0;
-        while(number!=0){
-            result++;
-            number = number&(number-1);
-        }
-        return result;
-    }
-    
-    /**
-     * 找到一个数组中只出现一次的两个数字，其他数字都出现了两次
-     * @param arr 
-     * @param num1 用来存储两个数字之一
-     * @param num2 用来存储两个数字之一
-     */
-    public static void findNumsApperOnce(int[] arr, int[] num1, int[] num2){
-        if(arr.length < 2)
-            return;
-        num1[0] = arr[0];
-        num2[0] = arr[1];
-        int temp = arr[0];
-        for(int i = 0;i<arr.length;i++){
-            temp = temp^arr[i];
-        }
-        int pos;
-        for(pos = 0;(temp&1)!=1;pos++)
-            temp = temp>>1;
-        for(int i=0;i<arr.length;i++){
-            if((arr[i]>>pos)==0)
-                num1[0]^=arr[i];
-            else
-                num2[0]^=arr[i];
-        }
-    }
-
-    /**
-     * 用查找一个数组中只出现过一次的数字，其他数字都出现了三次
-     * @param arr
-     * @return 返回该数字
-     */
-    public static int findNumberApperingOnce(int[] arr){
-        int[] bit = new int[32];
-        for(int i=0;i<arr.length;i++){
-            int temp = 1;
-            for(int j=0;j<32;j++){
-                bit[j]+=(arr[i]&(temp<<j))>>>j;
-            }
-        }
-        int res = 0;
-        for(int i=0;i<32;i++){
-            if(bit[i]%3==1)
-                res+=(1<<i);
-        }
-        return res;
     }
 }
